a20bf70ff83fb6102f6aaafdd0ab65d3ba2dea0e
Bill Mullholland <bill.mulholalnd@digital.justice.gov.uk>
2017-04-12 13:41:15 +0100

    MI-277 markdown issue corrected as per  https://github.com/Automattic/jetpack/issues/636

--- markdown/easy-markdown.php
+++ markdown/easy-markdown.php
@@ -429,7 +429,8 @@ class WPCom_Markdown {
 		}
 		// rejigger post_content and post_content_filtered
 		// revisions are already in the right place, except when we're restoring, but that's taken care of elsewhere
-		if ( 'revision' !== $post_data['post_type'] ) {
+		// prevent quick edit feature from overriding already-saved markdown (issue https://github.com/Automattic/jetpack/issues/636)
+      if ( 'revision' !== $post_data['post_type'] && ! isset( $_POST['_inline_edit'] ) ) {
 			$post_data['post_content_filtered'] = apply_filters( 'wpcom_untransformed_content', $post_data['post_content'] );
 			$post_data['post_content'] = $this->transform( $post_data['post_content'], array( 'id' => $post_id ) );
 			$post_data['post_content'] = apply_filters( 'content_save_pre', $post_data['post_content'] );
